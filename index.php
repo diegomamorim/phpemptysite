4d2c352e68196b41382722a6d75693ce6b3fc5cd
diegomamorim <diegomamorim@gmail.com>
2017-05-08 16:09:22 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-?>
+require_once 'Sinesp.php';
+$Placa = $_GET['Placa'];
+$veiculo = new Sinesp;
+
+try {
+    $veiculo->buscar($Placa);
+    if ($veiculo->existe()) {
+       // print_r($veiculo->dados());
+	echo json_encode($veiculo->dados());
+       // echo 'O ano do veiculo eh ' , $veiculo->anoModelo, PHP_EOL;
+    }
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
